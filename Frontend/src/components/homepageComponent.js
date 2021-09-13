b9b504fcd94093f64216c06b4b41e1327f9ebb6a
brand19 <stheven154@gmail.com>
2021-09-13 08:24:53 -0500

    otro chance

--- Frontend/src/components/homepageComponent.js
+++ Frontend/src/components/homepageComponent.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 
 class homepageComponent extends Component {
+  constructor (props){
+    super(props)
+    }
+
+    
   render() {
     return (
-      <div>
-        homepage component
-      </div>
+      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+  <button class="btn btn-primary me-md-2" type="button">Button</button>
+  <button class="btn btn-primary" type="button">Button</button>
+</div>
     )
-  }
-}
+   }
+   }
 
 export default homepageComponent
